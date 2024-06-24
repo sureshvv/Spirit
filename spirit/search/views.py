4872d30a52f224f8048e4478b2116d7ab45e2e2c
Suresh V <suresh@grafware.com>
2024-06-24 16:35:17 +0530

    click on search result and land up on comment

--- spirit/search/views.py
+++ spirit/search/views.py
@@ -3,8 +3,8 @@
 from haystack.views import SearchView as BaseSearchView
 from djconfig import config
 
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
+# from django.utils.decorators import method_decorator
+# from django.contrib.auth.decorators import login_required
 
 from ..comment.models import Comment
 from .forms import AdvancedSearchForm
@@ -35,11 +35,19 @@ class SearchView(BaseSearchView):
         if not s1:
             return None, []
         s1 = s1.lower()
-        comments = [
-            (x, y) for x in self.results
-            for y in Comment.objects.filter(topic_id=x.pk)
-            if y.comment.lower().find(s1) >= 0 and not y.is_removed
-        ]
+        comments = []
+        topic_comments = {}
+        all_ids = {}
+        for x in self.results:
+            if x.id not in topic_comments:
+                topic_comments[x.id] = Comment.objects.filter(
+                    topic_id=x.pk).order_by('date')
+            if x.id not in all_ids:
+                all_ids[x.id] = [z.id for z in topic_comments[x.id]]
+            for y in topic_comments[x.id]:
+                if y.comment.lower().find(s1) >= 0 and not y.is_removed:
+                    page_no = all_ids[x.id].index(y.id) // 20
+                    comments.append((x, y, page_no))
         paginator = None
         page0 = yt_paginate(
             comments,
@@ -50,9 +58,8 @@ class SearchView(BaseSearchView):
         for r in page0:
             d0 = dict(r[0].get_stored_fields(), title=r[1].comment)
             c_id = r[1].id
-            n_page = 1 + c_id // 20
-            if n_page > 1:
-                d0['slug'] += f'/?page={n_page}'
-            d0['slug'] += f'#c{c_id}'
+            slug = f'/?page={r[2]+1}' if r[2] > 0 else ''
+            slug += f'&pos={c_id}' if slug else f'/?pos={c_id}'
+            d0['slug'] += slug
             page.append({'fields': d0, 'pk': r[0].pk})
         return paginator, page
